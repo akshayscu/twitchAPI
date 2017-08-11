992809dba8ff1487ca6a48634c9e688e1c012588
Akshay Gadegone <akshay.gadegone@gmail.com>
2017-08-10 18:15:04 -0700

    checking fcc status

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  var api="https://api.twitch.tv/kraken/streams/dota2ti?client_id=cadz3awg3v7fa26rhtq95x61bjt4up";
+  console.log(api);
+  $.getJSON(api,function(data){
+    if(data.stream===null){
+      $('#fccStatus').text("FreeCodeCamp is Offline...").addClass("btn btn-default");
+      console.log("in if");
+    }
+    else{
+      $('#fccStatus').append('<a href="https://www.twitch.tv/freecodecamp">FreeCodeCamp is Online...</a>').addClass("btn btn-default");
+      console.log("in else");
+    }
+  });
+});
