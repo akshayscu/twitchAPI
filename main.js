d1c04cdae3b33a530ae833ec39febcae8ba948ac
Akshay Gadegone <akshay.gadegone@gmail.com>
2017-08-11 12:30:37 -0700

    fcc status

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
   //  },
    success:function(data){
      if(data.stream===null){
-       $('#fccStatus').text("FreeCodeCamp is Offline...").addClass("btn btn-default");
+       $('#fccStatus').text("FreeCodeCamp is Offline right now...").addClass("btn btn-default");
        console.log("in if");
      }
      else{
